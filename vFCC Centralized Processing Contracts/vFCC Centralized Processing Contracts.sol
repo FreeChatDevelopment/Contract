1ba793f2aa6ff27031787e121776ced37bbc09e3
Jack long <bnnvpnservice@gmail.com>
2023-03-18 20:09:41 +0800

    FIX

--- vFCC Centralized Processing Contracts/vFCC Centralized Processing Contracts.sol
+++ vFCC Centralized Processing Contracts/vFCC Centralized Processing Contracts.sol
@@ -1,88 +1,117 @@
-根据这份合约，写一个管理员转账的GO语言代码。
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-// 导入OpenZeppelin库的IERC20和Ownable接口
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
-// MySecuredContractWithAdminTransferRecord合约，继承自Ownable
-contract MySecuredContractWithAdminTransferRecord is Ownable {
-    // 声明_freechatCoin为IERC20接口类型，存储FreechatCoin代币合约的实例
-    IERC20 private _freechatCoin;
-
-    // 存放用户存款记录和管理员转账记录的结构体
-    struct Record {
-        uint256 depositAmount; // 用户存款数量
-        uint256 depositNonce; // 用户存款操作计数器
-        uint256 adminTransferAmount; // 管理员转账数量
-        uint256 adminTransferNonce; // 管理员转账操作计数器
-    }
-
-    // 存放用户存款记录和管理员转账记录的映射，键为地址，值为Record结构体实例
-    mapping(address => Record) private _records;
-
-    // 事件：用户存款
-    event Deposit(address indexed user, uint256 amount, uint256 nonce);
-    // 事件：管理员转账
-    event AdminTransfer(address indexed from, address indexed to, uint256 amount, uint256 nonce);
-
-    // 构造函数，初始化合约时传入FreechatCoin代币合约实例
-    constructor(IERC20 freechatCoin) {
-        _freechatCoin = freechatCoin;
-    }
-
-    // 存款函数，用户通过此函数向合约地址存入代币
-    function deposit(uint256 amount) external {
-        require(amount > 0, "Deposit amount must be greater than 0"); // 检查存款数量是否大于0
-
-        uint256 currentNonce = _records[msg.sender].depositNonce + 1; // 计算当前nonce值
-
-        // 更新用户存款记录
-        _records[msg.sender].depositAmount += amount;
-        _records[msg.sender].depositNonce = currentNonce;
-
-        // 将用户的代币转入合约地址
-        _freechatCoin.transferFrom(msg.sender, address(this), amount);
-
-        // 触发存款事件
-        emit Deposit(msg.sender, amount, currentNonce);
-    }
-
-    // 管理员转账函数，仅限管理员调用，用于从合约地址向其他地址转账代币
-    function adminTransfer(address to, uint256 amount) external onlyOwner {
-        require(amount > 0, "Transfer amount must be greater than 0"); // 检查转账数量是否大于0
-        require(to != address(0), "Invalid recipient address"); // 检查收款地址是否合法
-
-        uint256 currentNonce = _records[msg.sender].adminTransferNonce + 1; // 计算当前nonce值
-
-        // 更新管理员转账记录
-        _records[msg.sender].adminTransferAmount += amount;
-        _records[msg.sender].adminTransferNonce = currentNonce;
-
-        // 执行代币转账
-        _freechatCoin.transferFrom(msg.sender, to, amount);
-
-        // 触发管理员转账事件
-        emit AdminTransfer(msg.sender, to, amount, currentNonce);
-    }
-  
-// 获取用户的存款金额
-function getDepositAmount(address user) external view returns (uint256) {
-return _records[user].depositAmount;
+contract MySecuredContractWithAdminTransferRecord is AccessControl, ReentrancyGuard {
+using SafeMath for uint256;
+IERC20 private _freechatCoin;
+bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
+struct Record {
+    uint256 depositAmount;
+    uint256 depositNonce;
+    uint256 adminTransferAmount;
+    uint256 adminTransferNonce;
+    address recipient;
+    uint256 timestamp;
 }
 
-// 获取用户的存款操作次数
-function getDepositNonce(address user) external view returns (uint256) {
-    return _records[user].depositNonce;
+mapping(address => Record[]) private _records;
+
+uint256 private constant RECORDS_LIMIT = 100;
+
+event Deposit(address indexed user, uint256 amount, uint256 nonce);
+event AdminTransfer(address indexed from, address indexed to, uint256 amount, uint256 nonce);
+
+constructor(IERC20 freechatCoin) {
+    _freechatCoin = freechatCoin;
+    _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+    _setupRole(ADMIN_ROLE, msg.sender);
 }
 
-// 获取管理员的转账金额
-function getAdminTransferAmount(address admin) external view returns (uint256) {
-    return _records[admin].adminTransferAmount;
+function deposit(uint256 amount) external {
+    require(amount > 0, "Deposit amount must be greater than 0");
+    uint256 currentNonce = _records[msg.sender].length.add(1);
+    _records[msg.sender].push(Record(amount, currentNonce, 0, 0, address(0), block.timestamp));
+    emit Deposit(msg.sender, amount, currentNonce);
+    require(_freechatCoin.transferFrom(msg.sender, address(this), amount), "Deposit failed");
 }
 
-// 获取管理员的转账操作次数
-function getAdminTransferNonce(address admin) external view returns (uint256) {
-    return _records[admin].adminTransferNonce;
+function adminTransfer(address to, uint256 amount) external onlyRole(ADMIN_ROLE) nonReentrant {
+    require(amount > 0, "Transfer amount must be greater than 0");
+    require(to != address(0), "Invalid recipient address");
+    require(_records[msg.sender].length < RECORDS_LIMIT, "Too many records");
+    uint256 currentNonce = _records[msg.sender].length.add(1);
+    _records[msg.sender].push(Record(0, 0, amount, currentNonce, to, block.timestamp));
+    require(_freechatCoin.balanceOf(address(this)) >= amount, "Insufficient balance in contract");
+    require(_freechatCoin.allowance(owner(), address(this)) >= amount, "Contract is not authorized to transfer enough tokens");
+    require(_freechatCoin.transfer(to, amount), "Transfer failed");
+    emit AdminTransfer(msg.sender, to, amount, currentNonce);
+}
+
+function getDepositAmount(address user, uint256 nonce) public view returns (uint256) {
+    for (uint256 i = 0; i < _records[user].length; i++) {
+        if (_records[user][i].depositNonce == nonce) {
+            return _records[user][i].depositAmount;
+        }
+    }
+    revert("Record not found");
+}
+
+function getDepositNonce(address user) public view returns (uint256) {
+    return _records[user].length;
+}
+
+function getAdminTransferAmount(address admin, uint256 nonce) public view onlyRole(ADMIN_ROLE) returns (uint256) {
+    for (uint256 i = 0; i < _records[admin].length; i++) {
+        if (_records[admin][i].adminTransferNonce == nonce) {
+            return _records[admin][i].adminTransferAmount;
+        }
+    }
+    revert("Record not found");
+}
+
+function getAdminTransferNonce(address admin) public view onlyRole(ADMIN_ROLE) returns (uint256) {
+return _records[admin].length;
+}
+function getRecipient(address admin, uint256 nonce) public view onlyRole(ADMIN_ROLE) returns (address) {
+    for (uint256 i = 0; i < _records[admin].length; i++) {
+        if (_records[admin][i].adminTransferNonce == nonce) {
+            return _records[admin][i].recipient;
+        }
+    }
+    revert("Record not found");
+}
+
+function getBalance() external view onlyRole(DEFAULT_ADMIN_ROLE) returns (uint256) {
+    return _freechatCoin.balanceOf(address(this));
+}
+
+function getAllowance() external view onlyRole(DEFAULT_ADMIN_ROLE) returns (uint256) {
+    return _freechatCoin.allowance(owner(), address(this));
+}
+
+function withdraw(uint256 amount) external onlyRole(DEFAULT_ADMIN_ROLE) nonReentrant {
+    require(amount <= _freechatCoin.balanceOf(address(this)), "Insufficient balance in contract");
+    require(_freechatCoin.transfer(owner(), amount), "Withdrawal failed");
+}
+
+function emergencyWithdraw() external onlyRole(DEFAULT_ADMIN_ROLE) nonReentrant {
+    uint256 amount = _freechatCoin.balanceOf(address(this));
+    require(_freechatCoin.transfer(owner(), amount), "Emergency withdrawal failed");
+}
+
+function revokeAdminRole(address account) external onlyRole(DEFAULT_ADMIN_ROLE) {
+    revokeRole(ADMIN_ROLE, account);
+}
+
+function grantAdminRole(address account) external onlyRole(DEFAULT_ADMIN_ROLE) {
+    grantRole(ADMIN_ROLE, account);
+}
+
+function renounceAdminRole() external {
+    renounceRole(ADMIN_ROLE, msg.sender);
 }
