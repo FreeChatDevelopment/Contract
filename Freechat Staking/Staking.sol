de93c418c8fb3706f6c7cbd8eb12f88e47dfd342
Jack long <bnnvpnservice@gmail.com>
2023-03-19 01:24:13 +0800

    fix

--- Freechat Staking/Staking.sol
+++ Freechat Staking/Staking.sol
@@ -6,34 +6,21 @@ interface Token {
 }
 
 contract TokenStaking {
-    // 代币合约地址
-    address public tokenAddress;
-    // 代币总量
+    address public tokenAddress = 0x171b1daefac13a0a3524fcb6beddc7b31e58e079;
     uint public totalTokenAmount;
-    // 单个代币的收益
     uint public rewardPerToken;
-    // 上次更新时的总奖励金额
     uint public lastTotalRewardAmount;
-    // 合约管理员地址
     address public adminAddress;
-    // 账户信息
     mapping(address => uint) public balances;
     mapping(address => uint) public lastRewardAmounts;
-    mapping(address => uint) public lastUpdateTime;
+    mapping(address => uint) public depositTimestamps;
     mapping(address => bool) public isWhitelisted;
-    // 最小质押周期
-    uint public minStakeTime;
+    uint public minStakeTime = 360 days;
 
-    constructor(address _tokenAddress, uint _totalTokenAmount, uint _minStakeTime) {
-        tokenAddress = _tokenAddress;
-        totalTokenAmount = _totalTokenAmount;
-        rewardPerToken = 0;
-        lastTotalRewardAmount = 0;
+    constructor() {
         adminAddress = msg.sender;
-        minStakeTime = _minStakeTime;
     }
 
-    // 更新单个代币的收益
     function updateRewardPerToken() internal {
         uint currentTotalRewardAmount = address(this).balance;
         if (currentTotalRewardAmount > lastTotalRewardAmount && totalTokenAmount > 0) {
@@ -42,78 +29,77 @@ contract TokenStaking {
         lastTotalRewardAmount = currentTotalRewardAmount;
     }
 
-    // 质押代币
     function stake(uint amount) external {
         require(amount > 0, "Amount must be greater than zero.");
-        require(balances[msg.sender] == 0, "You have already staked tokens.");
         require(isWhitelisted[msg.sender], "You are not allowed to stake tokens.");
-        require(minStakeTime == 0 || lastUpdateTime[msg.sender] == 0 || block.timestamp >= lastUpdateTime[msg.sender] + minStakeTime, "You must stake your tokens for at least the minimum stake time.");
         updateRewardPerToken();
-        balances[msg.sender] = amount;
-        lastRewardAmounts[msg.sender] = amount * rewardPerToken;
-        lastUpdateTime[msg.sender] = block.timestamp;
+        balances[msg.sender] += amount;
+        lastRewardAmounts[msg.sender] = balances[msg.sender] * rewardPerToken;
+        depositTimestamps[msg.sender] = block.timestamp;
         totalTokenAmount += amount;
-        // Transfer tokens to this contract
         require(Token(tokenAddress).transferFrom(msg.sender, address(this), amount), "Token transfer failed.");
     }
 
-    // 提取质押代币和奖励
-    function withdraw() external {
+    function withdrawReward() external {
         require(balances[msg.sender] > 0, "You have not staked any tokens.");
-        require(minStakeTime == 0 || block.timestamp >= lastUpdateTime[msg.sender] + minStakeTime, "You must stake your tokens for at least the minimum stake time.");
+        require(block.timestamp >= depositTimestamps[msg.sender] + minStakeTime, "Rewards can only be withdrawn after the 360-day period has expired.");
         updateRewardPerToken();
-        uint rewardAmount = lastRewardAmounts[msg.sender] + (balances[msg.sender] * (rewardPerToken - lastRewardAmounts[msg.sender] / balances[msg.sender]));
-balances[msg.sender] = 0;
-lastRewardAmounts[msg.sender] = 0;
-lastUpdateTime[msg.sender] = 0;
-totalTokenAmount -= rewardAmount;
-// Transfer tokens and reward to the user
-require(Token(tokenAddress).transfer(msg.sender, balances[msg.sender]), "Token transfer failed.");
-require(payable(msg.sender).send(rewardAmount), "Reward transfer failed.");
+        uint rewardAmount = balances[msg.sender] * (rewardPerToken - lastRewardAmounts[msg.sender] / balances[msg.sender]);
+        lastRewardAmounts[msg.sender] = 0;
+        require(payable(msg.sender).send(rewardAmount), "Reward transfer failed.");
+    }
+
+    function withdrawPrincipal(uint amount) external {
+        require(balances[msg.sender] >= amount, "Insufficient balance.");
+        require(isWhitelisted[msg.sender], "You are not allowed to withdraw tokens.");
+        updateRewardPerToken();
+        balances[msg.sender] -= amount;
+        totalTokenAmount -= amount;
+        require(Token(tokenAddress).transfer(msg.sender, amount), "Token transfer failed.");
+    }
+
+    function addWhitelist(address account) external {
+        require(msg.sender == adminAddress, "Only the admin can add to the whitelist.");
+        isWhitelisted[account] = true;
+    }
+
+    function removeWhitelist(address account) external {
+        require(msg.sender == adminAddress, "Only the admin can remove from the whitelist.");
+        isWhitelisted[account] = false;
 }
 
-// 添加白名单
-function addWhitelist(address account) external {
-    require(msg.sender == adminAddress, "Only the admin can add to the whitelist.");
-    isWhitelisted[account] = true;
+function setAdminAddress(address newAdminAddress) external {
+    require(msg.sender == adminAddress, "Only the admin can change the admin address.");
+    adminAddress = newAdminAddress;
 }
 
-// 移除白名单
-function removeWhitelist(address account) external {
-    require(msg.sender == adminAddress, "Only the admin can remove from the whitelist.");
-    isWhitelisted[account] = false;
-}
-
-// 设置最小质押周期
-function setMinStakeTime(uint time) external {
-    require(msg.sender == adminAddress, "Only the admin can set the minimum stake time.");
-    minStakeTime = time;
-}
-
-// 向合约中添加代币
-function depositTokens(uint amount) external {
-    require(Token(tokenAddress).transferFrom(msg.sender, address(this), amount), "Token transfer failed.");
-    totalTokenAmount += amount;
-}
-
-// 从合约中提取代币
-function withdrawTokens(uint amount) external {
-    require(msg.sender == adminAddress, "Only the admin can withdraw tokens.");
-    require(totalTokenAmount >= amount, "Insufficient token balance.");
-    require(Token(tokenAddress).transfer(msg.sender, amount), "Token transfer failed.");
-    totalTokenAmount -= amount;
-}
-
-// 向合约中添加奖励
 function depositRewards() external payable {
     require(msg.sender == adminAddress, "Only the admin can deposit rewards.");
 }
 
-// 从合约中提取奖励
 function withdrawRewards() external {
     require(msg.sender == adminAddress, "Only the admin can withdraw rewards.");
     require(payable(msg.sender).send(address(this).balance), "Reward transfer failed.");
 }
 
-//注意，这个合约中使用了一个名为 `Token` 的接口，这个接口代表了一个 ERC-20 标准代币合约，如果你想使用这个合约，你需要在你的代码中导入一个实现了这个接口的代币合约，并把它的地址传递给这个合约的构造函数。另外，这个合约中的 `payable` 关键字用于接收和发送以太币，如果你不需要在这个合约中使用以太币，可以将它们移除。
+function getTotalStakedTokens() external view returns (uint) {
+    return totalTokenAmount;
+}
+
+function getStakedTokens(address account) external view returns (uint) {
+    return balances[account];
+}
+
+function getRewardAmount(address account) external view returns (uint) {
+    if (balances[account] == 0 || block.timestamp < depositTimestamps[account] + minStakeTime) {
+        return 0;
+    }
+    uint currentRewardPerToken = rewardPerToken;
+    uint currentTotalRewardAmount = address(this).balance;
+    if (currentTotalRewardAmount > lastTotalRewardAmount && totalTokenAmount > 0) {
+        currentRewardPerToken += (currentTotalRewardAmount - lastTotalRewardAmount) / totalTokenAmount;
+    }
+    return balances[account] * (currentRewardPerToken - lastRewardAmounts[account] / balances[account]);
+}
+
 
